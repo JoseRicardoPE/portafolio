7dfb1a33a5ab08d01f758346880b1e66b0c67ae4
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2024-04-27 16:26:32 -0500

    add interface info-pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,21 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: any;
+  info: InfoPagina = {};
   loaded = false;
 
   constructor( private http: HttpClient ) { 
 
     // Leer el archivo JSON
-    this.http.get('assets/data/data-page.json').subscribe( (response: any) => {
+    this.http.get('assets/data/data-page.json').subscribe( (response: InfoPagina) => {
       this.loaded = true;
       this.info = response;
-      console.log(response);
+      console.log(response.email);
     });
   }
 
